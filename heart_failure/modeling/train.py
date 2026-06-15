f720313099cb4039378a905d44645d07101a3aa8
lymuthien <tanya120606@mail.ru>
2026-06-15 18:04:02 +0300

    Selected metrics. Created functions to get all metrics. Built baseline.

---
+++ heart_failure/modeling/train.py
@@ -0,0 +1,20 @@
+import numpy as np
+import pandas as pd
+from sklearn.base import BaseEstimator, ClassifierMixin
+from heart_failure.config.features import CONJUNCTIVE_RULES
+from heart_failure.features import ConjRuleAggregator
+
+
+class ConjRuleBaseline(BaseEstimator, ClassifierMixin):
+    def __init__(self, conditions: dict, min_count: int = 50, min_target_rate: float = 0.9):
+        self.transformer = ConjRuleAggregator(conditions, min_count, min_target_rate)
+
+    def fit(self, X: pd.DataFrame, y) -> "ConjRuleBaseline":
+        self.transformer.fit(X, y)
+        return self
+
+    def predict(self, X: pd.DataFrame)-> np.ndarray:
+        X_transformed = self.transformer.transform(X)
+        y = X_transformed[CONJUNCTIVE_RULES].to_numpy()
+
+        return y
