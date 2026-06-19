4883244667f6e6451edb83b619c7b5e493cdfffa
lymuthien <tanya120606@mail.ru>
2026-06-19 13:35:44 +0300

    Renamed scoring.

--- heart_failure/config/modeling.py
+++ heart_failure/config/modeling.py
@@ -1,6 +1,8 @@
 CV_SPLITS = 5
 OPTUNA_N_TRIALS = 50
-LR_SCORING = "average_precision"
+CV_SCORING = "average_precision"
 F_BETA_THRESHOLD = 1.5
 
 DT_SCORING = "average_precision"
+
+CB_EVAL_METRIC = "PRAUC"
