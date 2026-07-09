726fd754f590f402cf7573f99162003d3720ff6a
lymuthien <tanya120606@mail.ru>
2026-07-09 12:03:46 +0300

    Remove loguru dependency

--- heart_failure/config/config.py
+++ heart_failure/config/config.py
@@ -1,14 +1,12 @@
 from pathlib import Path
 
 from dotenv import load_dotenv
-from loguru import logger
 
 # Load environment variables from .env file if it exists
 load_dotenv()
 
 # Paths
 PROJ_ROOT = Path(__file__).resolve().parents[2]
-logger.info(f"PROJ_ROOT path is: {PROJ_ROOT}")
 
 DATA_DIR = PROJ_ROOT / "data"
 RAW_DATA_DIR = DATA_DIR / "raw"
@@ -21,6 +19,7 @@ MODELS_DIR = PROJ_ROOT / "models"
 REPORTS_DIR = PROJ_ROOT / "reports"
 FIGURES_DIR = REPORTS_DIR / "figures"
 
+# Raw column names
 AGE = "Age"
 SEX = "Sex"
 CHEST_PAIN_TYPE = "ChestPainType"
